8bba4988885a4679793570fd121affc3e5f28f6c
Ivan Ruiz <ivanrur95@hotmail.com>
2022-07-26 14:28:52 -0500

    se hace comit inicial para alojar la primera version

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+addEventListener('DOMContentLoaded', () => {
+
+    const contadores = document.querySelectorAll('.contador_cantidad')
+    const velocidad = 1000
+
+    const animarContadores = () => {
+        for ( const contador of contadores) {
+            const actualizar_contador = () =>{
+                let cantidad_maxima = +contador.dataset.cantidadTotal,
+                valor_actual = +contador.innerText,
+                incremento = cantidad_maxima / velocidad
+
+                if (valor_actual < cantidad_maxima) {
+                    contador.innerText = Math.ceil(valor_actual + incremento)
+                    setTimeout(actualizar_contador, 10)
+                }else{
+                    contador.innerText = cantidad_maxima
+                }
+            }
+
+            actualizar_contador()
+        }
+
+    }
+    
+ 
+//     //IntersectionObserver
+
+    const mostrarContadores =  elementos => {
+        elementos.forEach(elemento => {
+            if(elemento.isIntersecting){
+                elemento.target.classList.add ('animar')
+                elemento.target.classList.remove('ocultar')
+                setTimeout(animarContadores,500)
+            }
+        });
+    }
+    const observer = new IntersectionObserver(mostrarContadores,{
+        threshold: 0.75 //0 - 1
+    }) 
+
+    const elementosHTML = document.querySelectorAll('.contador')
+    elementosHTML.forEach(elementoHTML =>{
+        observer.observe(elementoHTML)
+    }) 
+
+
+})
+
